a47d4bd73609f90d623db41380ae0e14351a3fc9
Gabriel Dias Moraes <gaabrielmooraes@gmail.com>
2020-02-01 15:49:26 -0300

    [New] [WIP] DropSystem.cs

---
+++ UnityProject/Assets/Scripts/Game/Player/Systems/DropSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+public class DropSystem : JobComponentSystem
+{
+    
+    private EntityQuery m_Query;
+    private BeginSimulationEntityCommandBufferSystem m_EntityCommandBuffer;
+    protected override void OnCreate()
+    {
+        m_EntityCommandBuffer = World.GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
+        
+    }
+    
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        JobHandle jobHandle = Entities
+            .WithAll<C_HoldComponentData>()
+            .ForEach((Entity entity, int entityInQueryIndex, in Translation translation, in C_HoldComponentData canPick, in MovementComponentData movementData) =>
+            {
+                
+            }).Schedule(inputDeps);
+        
+        
+        jobHandle.Complete();
+
+        return jobHandle;
+
+    }
+
+    
+    
+}
