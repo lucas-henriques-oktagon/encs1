a97b27ae9c96086f4d8eaef25796cf0952ca32d0
alandraz <59899059+alandraz@users.noreply.github.com>
2020-02-01 13:23:52 -0300

    Player Movement + Player Jump

---
+++ UnityProject/Assets/Scripts/Game/Player/Components/MovementComponentData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct MovementComponentData : IComponentData
+{
+    public float speed;
+    public int2 directionLook;
+}
